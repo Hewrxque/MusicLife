c297e6fd470914f4c25c91425c238d28728805fc
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-04-16 11:54:33 -0300

    Button convidate add

--- src/screens/Enter/styles.js
+++ src/screens/Enter/styles.js
@@ -1,56 +1,62 @@
 import {StyleSheet, Dimensions} from 'react-native';
 const {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
-    container:{
+  container: {
     flex: 1,
     height: height,
     width: width,
-    backgroundColor: '#FFFFFF'
-    },
+    backgroundColor: '#FFFFFF',
+  },
 
-    content:{
+  content: {
     flex: 0.5,
     marginHorizontal: 10,
     marginVertical: 10,
     justifyContent: 'flex-end',
-    alignItems: "center",
-  
-    },
-    content1:{
+    alignItems: 'center',
+  },
+  content1: {
     flex: 0.5,
     marginHorizontal: 10,
     marginVertical: 10,
-    alignItems: "center",
-   
-    },
-    image: {
-        width: "60%",
-        height: 235,
-      
-    },
-    buttonRegistration:{
-        backgroundColor: "#00FF00",
-        width: "90%",
-        height: 50,
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-    },buttonLogin:{
-        backgroundColor: "#FFFFFF",
-        width: "90%",
-        height: 50,
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-        borderColor:  "#00FF00",
-        borderWidth: 1
-    },
-    text:{
-        color: '#000',
-        fontSize: 16
-    }
-    
-})
+    alignItems: 'center',
+  },
+  image: {
+    width: '60%',
+    height: 235,
+  },
+  buttonRegistration: {
+    backgroundColor: '#00FF00',
+    width: '90%',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  buttonLogin: {
+    backgroundColor: '#FFFFFF',
+    width: '90%',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderColor: '#00FF00',
+    borderWidth: 1,
+  },
+  buttonConvidate: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  text: {
+    color: '#000',
+    fontSize: 16,
+  },
+  textConvidate: {
+    color: '#00FF00',
+    fontSize: 16,
+  },
+});
 export default styles;
