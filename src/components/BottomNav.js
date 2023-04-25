93daa8233535ae4cee3f6d70d4ee6fe2904bd9bd
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-04-25 12:02:17 -0300

    update

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,30 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function BottomNav({activepage}){
-    //console.log(activepage)
-    return(
-        <View style={styles.container}>
-            {activepage == 'allmusic' ?  <Icon name="music-box-multiple" size={50} color={'#fff'} style={styles.iconActivate}/> :  <Icon name="music-box-multiple" size={50} color={'#fff'} style={styles.icon}/>}
-            <Icon name="playlist-play" size={50} color={'#fff'}/>
-            <Icon name="headphones" size={50} color={'#fff'}/>
-           
-        </View>
-    )
+export default function BottomNav({activepage}) {
+  //console.log(activepage)
+  return (
+    <View style={styles.container}>
+      {activepage == 'allmusic' ? (
+        <Icon
+          name="music-box-multiple"
+          size={50}
+          color={'#fff'}
+          style={styles.iconActivate}
+        />
+      ) : (
+        <Icon
+          name="music-box-multiple"
+          size={50}
+          color={'#fff'}
+          style={styles.icon}
+        />
+      )}
+      <Icon name="playlist-play" size={50} color={'#fff'} />
+      <Icon name="headphones" size={50} color={'#fff'} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        alignItems: 'flex-end'
-    },
-    icon:{
-
-    },
-    iconActivate:{
-
-    }
-})
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end',
+  },
+  icon: {},
+  iconActivate: {},
+});
