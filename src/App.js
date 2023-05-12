e027965667b7044c66f5c6fd9f21c967964e2aa8
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-05-12 17:30:39 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
-import Login from './screens/setScreens/Login';
-import Enter from './screens/setScreens/Enter';
-import Registration from './screens/setScreens/Registration';
-import Splash from '../src/screens/Splash/splashScreen';
-import MusicsPlaylist from '../src/screens/MusicsPlaylist'
-
-
 import Route from "../src/routes/routes"
-
 
 export default function App() {
   return (
