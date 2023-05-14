45e7973d158526dce707c51c449a141751f07224
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-05-14 20:32:35 -0300

    update

--- src/screens/Player/index.js
+++ src/screens/Player/index.js
@@ -15,6 +15,7 @@ import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 import Slider from '@react-native-community/slider';
 import Sound from 'react-native-sound';
 import som from '../../sounds/set.mp3';
+
 export default function Player({navigation}) {
   const renderSongs = ({index, item}) => {
     return (
@@ -53,94 +54,76 @@ export default function Player({navigation}) {
   //Final da animacao da imagem
 
   // controle do play ou pause
-  const [isplaying, setisPlaying] = useState(true);
-  const sound = new Sound(som, Sound.MAIN_BUNDLE, error => {
-    if (error) {
-      console.log('Falha ao carregar o som', error);
-      return;
-    }
-    console.log('Loaded sound!');
-  });
+  const [isplaying, setisPlaying] = useState(false);
+  const [sound, setSound] = useState(new Sound(som, Sound.MAIN_BUNDLE));
+  const playIcon = <Icon name="play" size={50} color="#fff" />;
+  const pauseIcon = <Icon name="pause" size={50} color="#fff" />;
+  const [icon, setIcon] = useState(0);
 
-  function togglePlayPause() {
-    if (sound.isPlaying() == false) {
-      sound.play();
-      setisPlaying(true);
+  const handleSound = () => {
+    if (isplaying) {
+      sound.pause();
     } else {
-      sound.pause();
-      setisPlaying(false);
-    }
-  }
-  /*if(sound.isPlaying == true){
-      sound.pause();
-      setisPlaying(false)
-    }
-    else {
       sound.play();
-      setisPlaying(true);
     }
-  }*/
+    setisPlaying(!isplaying);
+  };
+
   return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.content}>
-          {/* área da imagem */}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        {/* área da imagem */}
 
-          <View style={styles.mainWrapper}>
-            <View style={[styles.imageWrapper, styles.elevationImage]}>
-              <Animated.Image
-                source={ImgMusic}
-                style={[styles.musicImage, {transform: [{rotate: RotateData}]}]}
-              />
-            </View>
-          </View>
-
-          {/* área de texto da música*/}
-          <View>
-            <Text style={[styles.songTitle, styles.songContent]}>
-              Akatsuki.mp4
-            </Text>
-            <Text style={[styles.songArtist, styles.songContent]}>
-              7 Minutoz
-            </Text>
-          </View>
-
-          {/* duraçao da música */}
-          <View>
-            <Slider
-              style={styles.progressBar}
-              value={10}
-              minimumValue={0}
-              maximumValue={100}
-              minimumTrackTintColor="#00FF00"
-              maximumTrackTintColor="#FFFFFF"
-              thumbTintColor="#00FF00"
-              onSlidingComplete={() => {}}
+        <View style={styles.mainWrapper}>
+          <View style={[styles.imageWrapper, styles.elevationImage]}>
+            <Animated.Image
+              source={ImgMusic}
+              style={[styles.musicImage, {transform: [{rotate: RotateData}]}]}
             />
-            <View style={styles.durationMusic}>
-              <Text style={styles.durationText}>00:00</Text>
-              <Text style={styles.durationText}>10:00</Text>
-            </View>
-          </View>
-          {/* área dos botoes de controle das músicas */}
-          <View style={styles.musicButtonControl}>
-            <TouchableOpacity>
-              <Icon2 name="skip-previous" size={60} color={'#fff'} />
-            </TouchableOpacity>
-            {/* funcao de controle do player*/}
-
-            <TouchableOpacity onPress={togglePlayPause}>
-              {isplaying ? (
-                <Icon name="play" size={50} color="#fff" />
-              ) : (
-                <Icon name="pause" size={50} color="#fff" />
-              )}
-            </TouchableOpacity>
-
-            <TouchableOpacity>
-              <Icon2 name="skip-next" size={60} color={'#fff'} />
-            </TouchableOpacity>
           </View>
         </View>
-      </SafeAreaView>
+
+        {/* área de texto da música*/}
+        <View>
+          <Text style={[styles.songTitle, styles.songContent]}>
+            Akatsuki.mp4
+          </Text>
+          <Text style={[styles.songArtist, styles.songContent]}>7 Minutoz</Text>
+        </View>
+
+        {/* duraçao da música */}
+        <View>
+          <Slider
+            style={styles.progressBar}
+            value={10}
+            minimumValue={0}
+            maximumValue={100}
+            minimumTrackTintColor="#00FF00"
+            maximumTrackTintColor="#FFFFFF"
+            thumbTintColor="#00FF00"
+            onSlidingComplete={() => {}}
+          />
+          <View style={styles.durationMusic}>
+            <Text style={styles.durationText}>00:00</Text>
+            <Text style={styles.durationText}>10:00</Text>
+          </View>
+        </View>
+        {/* área dos botoes de controle das músicas */}
+        <View style={styles.musicButtonControl}>
+          <TouchableOpacity>
+            <Icon2 name="skip-previous" size={60} color={'#fff'} />
+          </TouchableOpacity>
+          {/* funcao de controle do player*/}
+
+          <TouchableOpacity onPress={handleSound}>
+            {isplaying ? pauseIcon : playIcon}
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Icon2 name="skip-next" size={60} color={'#fff'} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
